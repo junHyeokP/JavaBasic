79546d9627f1ae7c8cae8567dec4ffd0f3f228c3
junHyeokP <162323599+junHyeokP@users.noreply.github.com>
2024-03-06 17:36:01 +0900

    수업 시간 코딩

---
+++ Print01.java
@@ -0,0 +1,17 @@
+package sec04;
+
+public class Print01 {
+
+	public static void main(String[] args) {
+	     String myName = "홍길동";
+	     int myAge = 20;
+	     
+	     System.out.println("이름 : " + myName);
+	     System.out.println("나이 : " + myAge);
+	     System.out.println();
+	     System.out.print("이름 : " + myName);
+         System.out.println("  나이 : " + myAge);
+
+	}
+
+}
