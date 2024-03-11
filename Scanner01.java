c9a42f8acd0d11901bf04cfd0e56fd17acaac9f7
junHyeokP <162323599+junHyeokP@users.noreply.github.com>
2024-03-11 16:09:53 +0900

    24년 3월 11 ~ 13일 과제

---
+++ Scanner01.java
@@ -0,0 +1,28 @@
+package scanner;
+
+import java.util.Scanner;
+
+public class Scanner01 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("정수 타입 숫자를 입력하세요 : ");
+		int a = input.nextInt();
+        System.out.println(a);
+		
+		System.out.print("실수 타입 숫자를 입력하세요 : ");
+        double b = input.nextDouble();
+        System.out.println(b);
+        
+//        System.out.println(a - b);
+//        System.out.println(a * b);
+//        System.out.println(a / b);
+        System.out.println(a + " + " + b + " = " + (a + b));
+        System.out.println(a + " - " + b + " = " + (a - b));
+        System.out.println(a + " * " + b + " = " + (a * b));
+        System.out.println(a + " / " + b + " = " + (a / b));
+        
+	}
+
+}
