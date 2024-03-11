2582756f5e39849f3693b7eab54cd12ca9e48ca8
junHyeokP <162323599+junHyeokP@users.noreply.github.com>
2024-03-11 16:18:30 +0900

    24년 3월 11 ~ 13일 과제

---
+++ Scanner02.java
@@ -0,0 +1,18 @@
+package scanner;
+
+import java.util.Scanner;
+
+public class Scanner02 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		final double PI = 3.14;
+		
+		System.out.print("원의 반지름을 입력하세요.(cm) : ");
+		int r = input.nextInt();
+		System.out.println(r);
+		
+		System.out.println("원의 넓이는 " + (r * r * PI) + "cm입니다." );
+	}
+
+}
