7b0c966b367bd54f68c9363ba76f94b1621a54e3
junHyeokP <162323599+junHyeokP@users.noreply.github.com>
2024-03-25 18:31:18 +0900

    24/03/25 반복문 break

--- Break01.java
+++ Break01.java
@@ -13,18 +13,21 @@ public class Break01 {
 	}
 	
 		public static void main(String[] args)throws IOException {
+			int num = 0;
+			int sum = 0;
 		while (true) {
-	    int sum = 0;
-		int num = Keyboard("숫자 입력 : ");
+	    sum = 0;
+		num = Keyboard("숫자 입력 : ");
 		if (num <= 0) {
 			System.out.println("0보다 큰 숫자를 입력하세요.");
-			break;
 		}
+		else {break;}
+	}
 			for (int i = 1; i <= num; i++) {
 				sum += i;
 			}
 			System.out.println("1부터 " + num + "까지의 합 : " + sum);
-		}
+		
 
 	}
 
