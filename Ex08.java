5d5367ae7a8ad166af02f4f391052b8b121d7c83
junHyeokP <162323599+junHyeokP@users.noreply.github.com>
2024-03-28 21:34:17 +0900

    24/03/28 쉽게 배우는 자바 프로그래밍 chap.03

--- Ex08.java
+++ Ex08.java
@@ -1,27 +1,19 @@
-package Exercise;
-
-import java.util.Scanner;
+package chap03;
 
 public class Ex08 {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.print("0 ~ 999 사이의 숫자를 입력하세요 : ");
-		int num = Integer.parseInt(sc.next());
-		int hundred = num/100;
-		int hundred2 = num%100;
-		int ten = hundred2/10;
-		int onecount = hundred2%10;;
-		
-		if (num >= 1000) {
-			System.out.println("1000을 넘겼습니다.");
-		}
-			
-		 
-				
-         System.out.println("각 자릿수의 합 = " + (hundred + ten + onecount) +" 입니다.");
-		
-   
-	}
+	    System.out.println(factorial(5));
 
+	}
+  static int factorial(int n) {
+      switch (n) {
+      case 5 : n *= 4;
+	  case 4 : n *= 3;
+	  case 3 : n *= 2;
+	  case 2 : n *= 1;
+      }	 
+	  return n;  
+	  }
+  
 }
--- Ex09.java
+++ Ex09.java
@@ -1,29 +1,21 @@
-package Exercise;
-
-import java.util.Scanner;
+package chap03;
 
 public class Ex09 {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.print("일반 이수 학점 : ");
-		int a = sc.nextInt();	
-		System.out.print("교양 이수 학점 : ");
-		int b = sc.nextInt();
-		System.out.print("일반 이수 학점 : ");
-		int c = sc.nextInt();
+		foo("Hi", 1);
+		foo("Hello", 1, 2);
+		foo("goodbye everyone");
 		
-		
-		if (a > 70) { 
-		   if(b > 30) {
-			 if(c > 30) {
-			    if(b + c > 80)
-			if (a + b + c > 140) {
-			System.out.println("졸업 가능");
-		}
-		} else {System.out.println("졸업 불가");}
-		} else {System.out.println("졸업 불가");}   
-		} else {System.out.println("졸업 불가");}
-	}
 
+	}
+	static void foo(String h, int n) {
+	System.out.println(h + " " + n);	
+	}
+	static void foo(String h, int n, int m) {
+		System.out.println(h + " " + n + " " + m);
+	}
+    static void foo(String h) {
+    	System.out.println(h);
+    }
 }
