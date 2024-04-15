b2b71f62c0eedf8902c7ce1144efa9fa57305254
junHyeokP <162323599+junHyeokP@users.noreply.github.com>
2024-04-15 21:04:48 +0900

    24/04/15 자바 강의 교안 예제 및 과제

---
+++ chap02/Print01.java
@@ -0,0 +1,21 @@
+package sec04;
+
+public class Print01 {
+
+	public static void main(String[] args) {
+	     String myName = "박준혁";
+	     int myAge = 24;
+	     
+	     System.out.println("이름 : " + myName);
+	     System.out.println("나이 : " + myAge);
+	     System.out.println();
+	     System.out.print("이름 : " + myName);
+         System.out.println("  나이 : " + myAge);
+
+         //println : 내용을 출력한 후 행을 바꾼다.
+         //print : 내용을 출력만 하고 행은 바꾸지 않는다.
+         //printf : 문자열의 포맷을 지정해서 출력한다.
+               	
+	}
+
+}
---
+++ chap02/thejav.java
@@ -0,0 +1,17 @@
+package sec04;
+
+import java.util.Scanner;
+
+public class thejav {
+       public static void main(String[] args) {
+       Scanner input = new Scanner(System.in);		
+      
+       System.out.print(">>팩토리얼 계산기 : ");
+       int f = input.nextInt();
+       int num = f;
+       System.out.println(f *= --num );
+      
+        		
+	}
+
+}
